b27a4d3c663239895294ec1d5ff1c1c591e8d0e4
SuYu鄭 <suyu_cheng@gmail.com>
2018-05-21 13:43:50 +0800

    commit

---
+++ project01.py
@@ -0,0 +1,12 @@
+name = "Cook"
+print("My name is {}".format(name))
+
+for i in range(1,22,2):
+    if i < 10:
+        print("0~10")
+    elif i < 20:
+        print("10~20")
+    else:
+        print(i)
+
+print("儲存第三次囉~~")
